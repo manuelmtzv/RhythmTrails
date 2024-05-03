984febad66dc5061fee1849ae85083da3cbb7a59
Manuel Martínez <manuel.mtzv816@gmail.com>
2024-05-03 00:31:50 -0600

    feat: add structure to spotify auth error page

--- src/app/errors/spotify-auth-error/page.tsx
+++ src/app/errors/spotify-auth-error/page.tsx
@@ -1,4 +1,5 @@
 import { retriveAuthError } from "@/actions/spotify/auth.actions";
+import Link from "next/link";
 import { redirect } from "next/navigation";
 
 export default async function SpotifyAuthErrorPage() {
@@ -10,8 +11,16 @@ export default async function SpotifyAuthErrorPage() {
 
   return (
     <main className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-4xl font-bold">Error</h1>
-      <p>{error}</p>
+      <h1 className="text-4xl font-bold">
+        There was an error while trying to login with your Spotify Account
+      </h1>
+      <p className="text-lg">
+        Please try again. If the problem persists, please contact us.
+      </p>
+
+      <Link href="/">Return to home</Link>
+
+      <span>Server error: {error}</span>
     </main>
   );
 }
